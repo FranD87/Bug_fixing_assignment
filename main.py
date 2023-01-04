9e59049b3d09059e98832b9342d82db9e450448d
FranD87 <fdunne007@gmail.com>
2023-01-04 15:36:46 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Fix the fizz buzz
+#
+# for n in range(1, 100):
+#
+#     if n % 3 == 0 and n % 5 == 0:
+#         print('Fizzbuzz')
+#
+#     if n % 3 == 0:
+#         print('Fizz')
+#
+#     elif n % 5 == 0:
+#         print('Buzz')
+#
+#     else:
+#         print(n)
+
+
