b14df1bd041e1b8dc36250268b94b2d98a0be226
FranD87 <fdunne007@gmail.com>
2023-01-09 10:47:46 +0100

    Fran assign

---
+++ bug.py
@@ -0,0 +1,116 @@
+# Fix the fizz buzz
+#
+# for n in range(1, 100):
+#
+#     if n % 3 == 0 and n % 5 == 0:
+#         print('Fizzbuzz')
+#
+#     if n % 3 == 0:
+#         print('Fizz')
+#
+#     elif n % 5 == 0:
+#         print('Buzz')
+#
+#     else:
+#         print(n)
+
+# Task 2-summing integers
+# sum integers from 1 - 5 using while loop
+# calculate what the result should be and what you get after running the program
+#
+# sum_of_numbers = 0
+#
+# num = 5
+#
+# while num > 0:
+#     sum_of_numbers += num
+#
+#     num -= 1
+# print(sum_of_numbers)
+
+# Task 3 - summing integers with rang()
+# *Sum integers from 1 to 5 using the range() funtion*
+
+# n = 1
+# num = 6
+#
+# total = sum(range(n, num))
+#
+#
+# print(total)
+
+# Task 4 - printing in loops
+
+# Program 1:
+
+# for x in range(0, 3):
+#     print(x)
+#
+# Program 2:
+
+# for j in range(0, 5):
+#     print("This is loop number", j)
+#
+# Program 3:
+
+# x = 11
+#
+# while x > 0:
+#     x -= 1
+#     print(x)
+
+# program 4:
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+# print("Start!")
+
+# Task 5 - Summing three integers!!!!!
+# import math
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+# z = int(input("Third number: "))
+#
+# if x == y or y == z:
+#     result = 0
+# else:
+#     result = x+y+z
+#
+# print("Calculated sum is:", result)
+
+# Task 6 - Summing two Integers
+
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+#
+# result = x + y
+#
+# if result >= 15 or result <= 20:
+#     result = 20
+#
+# print("Calculated sum is: ", result)
+
+# Task 7 - Swapping variables
+
+# a = input("First value: ")
+# b = input("Second value: ")
+#
+# temp = a
+# a = b
+# b = temp
+#
+# print("Before swapping: a = {}".format(a))
+# print("After Swapping: b = {}".format(b))
+
+# Task 8 - max and min values
+
+# a = float(input("First number: "))
+# b = float(input("Second number: "))
+# c = float(input("Third number: "))
+#
+# print("Max number =", max(a, b, c))
+# print("min number =", min(a, b, c))
+
+# Task 9 - convertion
